fdced6e6f092ab7c1b861174c8aef72ecfb9c9cc
jstand <akiricuk37@gmail.com>
2023-05-25 19:33:48 +0200

    Movement for snake,game panel

---
+++ src/main/java/org/example/GameFrame.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+        public GameFrame() {
+            this.add(new GamePanel());
+            this.setTitle("SnakeGame");
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.setResizable(false);
+            this.pack();
+            this.setLocationRelativeTo(null);
+            this.setVisible(true);
+        }
+    }
